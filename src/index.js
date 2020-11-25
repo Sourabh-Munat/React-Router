21bf84322c44dd1faa8d904a1368690db8c103b5
Sourabh Jain <sourabh.jain@calsoftinc.com>
2020-11-25 19:36:56 +0530

    Added redux, redux-form and initial navigation.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
+import reducers from "./reducers";
+
+// added to enable redux devtools
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  // // added to enable redux devtools and can add middleware
+  composeEnhancers(applyMiddleware())
+);
+/* eslint-enable */
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
   </React.StrictMode>,
   document.getElementById("root")
 );
